58414071257f6bd009482ec74d55d6b746dac9c6
sueun <songtndms@mqnic.com>
2024-04-17 14:29:20 +0900

    init commit

---
+++ src/pages/MainPage.js
@@ -0,0 +1,10 @@
+import BasicLayout from "../layouts/BasicLayout";
+const MainPage = () => {
+    return (
+        <BasicLayout>
+            <div className=" text-3xl">Main Page</div>
+        </BasicLayout>
+    );
+}
+
+export default MainPage;
